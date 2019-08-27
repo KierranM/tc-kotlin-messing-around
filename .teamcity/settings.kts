e97a5c8ba52a93567f2adb5de56552a28c9f495c
Kierran McPherson <kierranm@gmail.com>
2019-08-27 18:29:13 +1200

    Christmas only step

---
+++ .teamcity/settings.kts
@@ -0,0 +1,31 @@
+import jetbrains.buildServer.configs.kotlin.v2018_2.*
+import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.script
+import java.time.LocalDate
+import java.time.Month
+
+version = "2019.1"
+
+project {
+    buildType(ChristmasBuild)
+}
+
+object ChristmasBuild : BuildType({
+    name = "Hello World"
+    steps {
+        val date = LocalDate.now()
+        if (date.month == Month.DECEMBER && date.dayOfMonth == 25) {
+            script {
+                name = "Merry Christmas"
+                scriptContent = """
+                    echo "MERRY CHRISTMAS!!"
+                """.trimIndent()
+            }
+        }
+        script {
+            name = "Hello World"
+            scriptContent = """
+                echo "hello world! I'm a build"
+            """.trimIndent()
+        }
+    }
+})
