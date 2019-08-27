e1aea3dcc647f35fe4bed234e45c3f6a43e5a237
kierranm <kierranm@TeamCity>
2019-08-27 06:33:36 +0000

    TeamCity change in 'TeamCity Kotlin Examples / Messing Around' project: Versioned settings configuration updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,31 +1,46 @@
 import jetbrains.buildServer.configs.kotlin.v2018_2.*
 import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.script
-import java.time.LocalDate
-import java.time.Month
+
+/*
+The settings script is an entry point for defining a TeamCity
+project hierarchy. The script should contain a single call to the
+project() function with a Project instance or an init function as
+an argument.
+
+VcsRoots, BuildTypes, Templates, and subprojects can be
+registered inside the project using the vcsRoot(), buildType(),
+template(), and subProject() methods respectively.
+
+To debug settings scripts in command-line, run the
+
+    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
+
+command and attach your debugger to the port 8000.
+
+To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
+-> Tool Windows -> Maven Projects), find the generate task node
+(Plugins -> teamcity-configs -> teamcity-configs:generate), the
+'Debug' option is available in the context menu for the task.
+*/
 
 version = "2019.1"
 
 project {
+
     buildType(ChristmasBuild)
 }
 
 object ChristmasBuild : BuildType({
     name = "Hello World"
+
     steps {
-        val date = LocalDate.now()
-        if (date.month == Month.AUGUST && date.dayOfMonth == 27) {
-            script {
-                name = "Merry Christmas"
-                scriptContent = """
-                    echo "MERRY CHRISTMAS!!"
-                """.trimIndent()
-            }
+        script {
+            name = "Merry Christmas"
+            scriptContent = """echo "MERRY CHRISTMAS!!""""
         }
         script {
             name = "Hello World"
-            scriptContent = """
-                echo "hello world! I'm a build"
-            """.trimIndent()
+            scriptContent = """echo "hello world! I'm a build""""
         }
     }
 })
